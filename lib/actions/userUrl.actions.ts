8cf75a655cc000728ff3efe97deac9cd6c4ab5a1
DESKTOP-UIMGBSJ\Valentin <menvielle.valen@gmail.com>
2024-01-29 17:24:16 -0300

    add path and model

--- lib/actions/userUrl.actions.ts
+++ lib/actions/userUrl.actions.ts
@@ -1,12 +1,16 @@
 "use server";
 
+import Url from "../models/url.model";
 import UserUrl from "../models/user-url";
 import { connectToDB } from "../mongoose";
 
 export const findUserUrls = async (userEmail: string): Promise<any[]> => {
   await connectToDB();
   try {
-    const userUrls = await UserUrl.find({ userEmail }).populate("url").exec();
+    const userUrls = await UserUrl.find({ userEmail }).populate({
+      path: 'url',
+      model: Url
+    }).exec();
     return userUrls;
   } catch (error) {
     console.log(error);
