6761591d88d0beaf6ec3bd2a124f9d2518c48d66
DESKTOP-UIMGBSJ\Valentin <menvielle.valen@gmail.com>
2024-01-29 19:25:16 -0300

    links lowercase

--- lib/validations/url.ts
+++ lib/validations/url.ts
@@ -2,8 +2,8 @@ import * as z from 'zod';
 
 
 export const UrlSchema = z.object({
-    longUrl: z.string().url().min(1),
-    shortUrl: z.string().min(1).max(8),
+    longUrl: z.string().url().min(1).toLowerCase(),
+    shortUrl: z.string().min(1).max(8).toLowerCase(),
     description: z.string().max(200),
     userEmail: z.string().email().min(1)
 })
